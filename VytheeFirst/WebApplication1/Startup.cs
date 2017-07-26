4820e0cd90f089969eea161c04036f9235e17780
Vytheeswaran Jeganathan <9500078@CARMAX.COM>
2017-07-26 09:48:02 -0400

    Removed bower and configured gulp for lib
    Lib folder setup with gulp

--- VytheeFirst/WebApplication1/Startup.cs
+++ VytheeFirst/WebApplication1/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace WebApplication1
 {
@@ -47,14 +48,28 @@ namespace WebApplication1
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if( context.Response.StatusCode == 404 && ! Path.HasExtension(context.Request.Path.Value) &&
+                !context.Request.Path.Value.StartsWith("/api/"))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
+
+            
+            app.UseMvcWithDefaultRoute();
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
         }
     }
 }
---
+++ VytheeFirst/WebApplication1/gulpfile.js
@@ -0,0 +1,55 @@
+﻿/// <binding BeforeBuild='copy-all' />
+/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+"use strict";
+var gulp = require("gulp");
+var root_path = {
+    webroot: "./wwwroot/"
+};
+//library source  
+root_path.nmSrc = "./node_modules/";
+//library destination  
+root_path.package_lib = root_path.webroot + "lib/";
+root_path.package_css = root_path.webroot + "css/";
+
+gulp.task("copy-jquery", function () {
+    return gulp.src([
+        root_path.nmSrc + '/jquery/dist/**/*.js',
+        root_path.nmSrc + '/jquery-validation/dist/*.js'
+]).pipe(gulp.dest(root_path.package_lib + '/jquery/'));
+});
+
+
+
+gulp.task("copy-systemjs", function () {
+    return gulp.src(root_path.nmSrc + '/systemjs/dist/**/*.*', {
+        base: root_path.nmSrc + '/systemjs/dist/'
+    }).pipe(gulp.dest(root_path.package_lib + '/systemjs/'));
+});
+
+gulp.task("copy-angular", function () {
+    return gulp.src(root_path.nmSrc + '/angular/**/*.js', {
+        base: root_path.nmSrc + '/angular/'
+    }).pipe(gulp.dest(root_path.package_lib + '/angular/'));
+});
+gulp.task("copy-es6-shim", function () {
+    return gulp.src(root_path.nmSrc + '/es6-shim/es6-sh*', {
+        base: root_path.nmSrc + '/es6-shim/'
+    }).pipe(gulp.dest(root_path.package_lib + '/es6-shim/'));
+});
+gulp.task("copy-rxjs", function () {
+    return gulp.src(root_path.nmSrc + '/rxjs/bundles/*.*', {
+        base: root_path.nmSrc + '/rxjs/bundles/'
+    }).pipe(gulp.dest(root_path.package_lib + '/rxjs/'));
+});
+
+gulp.task("copy-bootstrap_js", function () {
+    return gulp.src(root_path.nmSrc + '/bootstrap/dist/**', {
+        base: root_path.nmSrc + '/bootstrap/'
+    }).pipe(gulp.dest(root_path.package_lib + '/bootstrap/'));
+});
+
+gulp.task("copy-all", ["copy-rxjs",'copy-jquery','copy-bootstrap_js','copy-angular', 'copy-systemjs', 'copy-es6-shim']);
